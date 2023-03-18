61d7ceea8ce4ddd99e821968186b0b1b8df750b2
Abhijit Rao <abhijit945@gmail.com>
2023-03-18 14:32:54 -0500

    fix: try removing _document

--- theme.config.js
+++ theme.config.js
@@ -25,50 +25,6 @@ export default {
   feedback: {
     content: null,
   },
-  head: () => {
-    const title = 'Portfolio - Abhijit Rao';
-    const description = `Brain dump`;
-    const image = '/icons/main.svg';
-    return (
-      <>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
-        />
-        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/favicon/favicon.svg" color="#000000" />
-        <link rel="shortcut icon" href="/favicon/favicon.ico" />
-        <link rel="icon" type="image/svg+xml" href="/favicon/favicon-32x32.png" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="apple-mobile-web-app-title" content="Abhijit Rao's Blog" />
-        <meta name="robots" content="follow, index" />
-        <meta name="description" content={description} />
-        <meta property="og:site_name" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:title" content={title} />
-        <meta property="og:image" content={image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@AbhijitRao_" />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={image} />
-      </>
-    );
-  },
   logo: () => {
     return <MainLogo width={'100%'} height={24} />;
   },
