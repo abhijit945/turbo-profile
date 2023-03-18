cb15cbf1e0fcdec0f6add4f792b8c8544b2635b2
Abhijit Rao <arao@Abhijits-MacBook-Pro.local>
2023-03-18 13:59:57 -0500

    feat: Add threejs docs

--- theme.config.js
+++ theme.config.js
@@ -34,25 +34,25 @@ export default {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href="/favicon/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
+          href="/favicon/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png"
+          href="/favicon/favicon-16x16.png"
         />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/favicon.svg" color="#000000" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="icon" type="image/svg+xml" href="/favicon-32x32.png" />
+        <link rel="mask-icon" href="/favicon/favicon.svg" color="#000000" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon-32x32.png" />
         <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="apple-mobile-web-app-title" content="Abhijit Rao's Blog" />
         <meta name="robots" content="follow, index" />
